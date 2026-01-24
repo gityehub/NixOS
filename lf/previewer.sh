9b1db7bb9d1263c6499632a38816b5e6a081f835
Tye <apps@jliiman.com>
2026-01-24 17:59:01 +0200

    Update previewer.sh

--- lf/previewer.sh
+++ lf/previewer.sh
@@ -1,32 +1,17 @@
 #!/bin/sh
-draw() {
-  kitten icat --stdin no --transfer-mode memory --place "${w}x${h}@${x}x${y}" "$1" </dev/null >/dev/tty
-  exit 1
-}
 
-file="$1"
-w="$2"
-h="$3"
-x="$4"
-y="$5"
+draw(){ kitten icat --stdin no --transfer-mode memory \
+  --place "${w}x${h}@${x}x${y}" "$1" </dev/null >/dev/tty; exit 1; }
 
-case "$(file -Lb --mime-type "$file")" in 
-  image/*)
-    draw "$file"
-    ;;
+file=$1 w=$2 h=$3 x=$4 y=$5
+mime=$(file -Lb --mime-type "$file")
+
+case $mime in
+  image/*) draw "$file" ;;
   video/*)
-  thumb="$(mktemp --suffix=.png)"
-    ffmpegthumbnailer \
-      -i "$file" \
-      -o "$thumb" \
-      -s 0 \
-      -t 10% \
-      -q 8
-    draw "$thumb"
-    rm -f "$thumb"
+    t=$(mktemp --suffix=.png)
+    ffmpegthumbnailer -i "$file" -o "$t" -s 0 -t 10% -q 8
+    draw "$t"; rm -f "$t"
     ;;
-  *)
-    bat --color=always --style=plain "$file" 2>/dev/null \
-      || sed -n '1,200p' "$file"
-    ;;
-esac 
+  *) bat --color=always --style=plain "$file" 2>/dev/null || sed -n '1,200p' "$file" ;;
+esac
